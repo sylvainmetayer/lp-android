12ff072f877b98f28c290f14817401608f8358bf
sylvain METAYER <sylvain.metayer@outlook.fr>
2017-04-12 13:56:02 +0200

    Not working

--- IntentImplicite/app/src/main/java/com/example/smetayer/intentimplicite/MainActivity.java
+++ IntentImplicite/app/src/main/java/com/example/smetayer/intentimplicite/MainActivity.java
@@ -4,13 +4,17 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int MY_PERMISSION_TO_CALL = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +26,6 @@ public class MainActivity extends AppCompatActivity {
         call.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent call = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "0643738482"));
                 if (ActivityCompat.checkSelfPermission(v.getContext(), Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
                     // TODO: Consider calling
                     //    ActivityCompat#requestPermissions
@@ -31,11 +34,41 @@ public class MainActivity extends AppCompatActivity {
                     //                                          int[] grantResults)
                     // to handle the case where the user grants the permission. See the documentation
                     // for ActivityCompat#requestPermissions for more details.
+                    ActivityCompat.requestPermissions(MainActivity.super.getParent(),
+                            new String[]{Manifest.permission.CALL_PHONE},
+                            MY_PERMISSION_TO_CALL);
+
+                    Intent call = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "0643738482"));
+                    startActivity(call);
                     return;
                 }
-                startActivity(call);
+
             }
         });
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        switch (requestCode) {
+            case MainActivity.MY_PERMISSION_TO_CALL: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.i("CALL", "User granted permission");
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+                    Log.wtf("CALL", "User denied call");
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
 
     }
 }
