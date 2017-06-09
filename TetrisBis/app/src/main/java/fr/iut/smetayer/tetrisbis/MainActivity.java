d91e9f104668524cfa8d6b1d9b91e79529f6ce6c
Sylvain Metayer <sylvain_metayer@outlook.fr>
2017-06-09 21:12:54 +0200

    Refactor MainActivity

--- TetrisBis/app/src/main/java/fr/iut/smetayer/tetrisbis/MainActivity.java
+++ TetrisBis/app/src/main/java/fr/iut/smetayer/tetrisbis/MainActivity.java
@@ -2,7 +2,6 @@ package fr.iut.smetayer.tetrisbis;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -11,7 +10,6 @@ import android.widget.GridView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -21,9 +19,11 @@ import fr.iut.smetayer.tetrisbis.metier.pieces.Piece_I;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Game game_board;
+    private Game game;
     private GridView layout;
     private EditText editText;
+
+    private ArrayList<Piece> datas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,53 +34,23 @@ public class MainActivity extends AppCompatActivity {
         int nb_lines = getResources().getInteger(R.integer.maxLines);
 
         layout = (GridView) findViewById(R.id.grid);
-        ArrayList<Piece> datas = new ArrayList<>();
-
-        int[][] matrice =
-                {
-                        {1},
-                        {1},
-                        {1}
-                };
-        Piece start_piece = new Piece_I(matrice, 0, 2, this);
-        datas.add(start_piece);
-
-        game_board = new Game(datas, nb_lines, nb_columns);
-
-        final Button pause = (Button) findViewById(R.id.pause);
-        pause.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                game_board.togglePause();
-                if (game_board.isPause()) {
-                    pause.setText(getResources().getString(R.string.restart));
-                } else {
-                    pause.setText(getResources().getString(R.string.pause));
-                }
-            }
-        });
-
         editText = (EditText) findViewById(R.id.score);
 
-        MyAdapter adapter = new MyAdapter(MainActivity.this, R.layout.item, game_board.getGameboard());
+
+        initDatas();
+        game = new Game(datas, nb_lines, nb_columns);
+
+        MyAdapter adapter = new MyAdapter(MainActivity.this, R.layout.item, game.getGameboard());
         layout.setAdapter(adapter);
 
-
-        // Set mouvement detection on grid.
-        layout.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(MainActivity.this, "click", Toast.LENGTH_SHORT).show();
-            }
-        });
-        layout.setOnTouchListener(new OnSwipeTouchListenerImpl(this));
+        initListeners();
 
         Timer timer = new Timer();
         timer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
-                if (!MainActivity.this.game_board.isPause()) {
-                    game_board.performAction(MainActivity.this);
+                if (!MainActivity.this.game.isPause()) {
+                    game.performAction(MainActivity.this);
                     MainActivity.this.refresh();
                 }
 
@@ -93,12 +63,46 @@ public class MainActivity extends AppCompatActivity {
         this.runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                //adapter.notifyDataSetChanged();
-                //adapter.clear();
-                // adapter.addAll(game_board.getGameboard()); // Crash the app
-                layout.setAdapter(new MyAdapter(MainActivity.this, R.layout.item, game_board.getGameboard()));
-                editText.setText(game_board.getScore());
+                //adapter.notifyDataSetChanged(); // Isn't working :(
+                layout.setAdapter(new MyAdapter(MainActivity.this, R.layout.item, game.getGameboard()));
+                editText.setText(game.getScore());
             }
         });
+    }
+
+    public void initListeners() {
+
+        final Button pause = (Button) findViewById(R.id.pause);
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                game.togglePause();
+                if (game.isPause()) {
+                    pause.setText(getResources().getString(R.string.restart));
+                } else {
+                    pause.setText(getResources().getString(R.string.pause));
+                }
+            }
+        });
+
+        layout.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "click", Toast.LENGTH_SHORT).show();
+            }
+        });
+        layout.setOnTouchListener(new OnSwipeTouchListenerImpl(this));
+    }
+
+    public void initDatas() {
+        datas = new ArrayList<>();
+        int[][] matrice =
+                {
+                        {1},
+                        {1},
+                        {1}
+                };
+        Piece start_piece = new Piece_I(matrice, 0, 2, this);
+        datas.add(start_piece);
     }
 }
